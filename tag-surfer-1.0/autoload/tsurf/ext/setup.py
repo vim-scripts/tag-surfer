7bda12993de925f0d244038d5e941f8fc7f2f39b
Able Scraper <scraper@vim-scripts.org>
2014-01-28 20:54:19 -0600

    Renamed to surfer.vim

--- tag-surfer-1.0/autoload/tsurf/ext/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-
-setup(
-    ext_modules = [
-        Extension('search', sources = ['searchmodule.c'])
-    ]
-)
--- tag-surfer-1.0/complete-installation.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd ./autoload/tsurf/ext
-python setup.py build_ext --inplace
-rm -fR build
